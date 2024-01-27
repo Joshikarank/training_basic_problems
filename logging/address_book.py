c2ad004d13ca6c518ede1310dc78fbd99a069e14
Joshi Karan <joshikarank2002@gmail.com>
2024-01-27 11:11:13 +0530

    [joshikaran] - started the problem

---
+++ logging/address_book.py
@@ -0,0 +1,6 @@
+import logging
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
+logging.debug('debug meesage in the file')
+logging.info('information in log file')
+logging.warning('warning message inlog')
+logging.error('error messages')
