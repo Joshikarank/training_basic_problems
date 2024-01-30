b53272edabcd5fcb03e6f06990340952010eb767
Joshi Karan <joshikarank2002@gmail.com>
2024-01-30 10:32:49 +0530

    [joshi] - higher order functions

--- higher_order_functions/higher_order_functions.py
+++ higher_order_functions/higher_order_functions.py
@@ -1,11 +1,11 @@
+# sample higher order functions
 def caps(text):
     return text.upper()
-
 def low(text):
     return text.lower()
-
 print(caps("hellow wor;d"))
 print(low("HELLOWORLD"))
+
 
 def divisor(x):
     def divident(y):
@@ -14,14 +14,15 @@ def divisor(x):
 divide= divisor(10)
 print(divide(2))
 
+
+
+# Decorator functions
 def sample(function1):
     def display():
         print("this comes  from the outer function")
         function1()
         print("this  also comes from the outer function")
     return display()
-
 def btween():
     print("this comes between")
-
 disp = sample(btween)
